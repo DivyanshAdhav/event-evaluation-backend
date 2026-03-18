f79bb46e6677c44e24c6eadb970dd5e1147129f7
Divyansh <adhavdivyansh@gmail.com>
2026-03-18 19:25:34 +0530

    Frontend URL updated

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(cors({
-  origin: "https://event-evaluation-frontend-3fa4.vercel.app",
+  origin: "https://event-evaluation-frontend-jc6j.vercel.app",
   credentials: true
 }))
 app.use(express.json())
